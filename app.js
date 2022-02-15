a5c589da6b6c752f829f427478afbe890ec0a8cb
hingbelieve@163.com <Hingbelieve@163.com>
2022-02-15 23:34:30 +0800

    commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const http = require('http')
+const querystring = require('querystring')
+
+
+const server = http.createServer((req, res) => {
+  const method = req.method
+  const url = req.url
+  const path = url.split('?')[0]
+  const query = querystring.parse(url.split('?')[1])
+
+  res.setHeader('Content-Type', 'application/json')
+
+  const resData = {
+    method,
+    url,
+    path,
+    query
+  }
+
+  if (method === 'GET') {
+    res.end(JSON.stringify(resData))
+  }
+
+  if (method === 'POST') {
+    let chunkData = ''
+    req.on('data', chunk => {
+      chunkData += chunk.toString()
+    })
+    req.on('end', () => {
+      resData.postData = chunkData
+      res.end(JSON.stringify(resData))
+    })
+  }
+})
+
+server.listen(3000)
