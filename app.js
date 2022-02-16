5d45ffbf1d61224d032aff5d005e209cde817bc2
hingbelieve@163.com <Hingbelieve@163.com>
2022-02-16 23:18:11 +0800

    init

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-const http = require('http')
-const querystring = require('querystring')
+const querystring = require('querystring');
 
+const serverHandler = (req, res) => {
+  const method = req.method;
+  const url = req.url;
+  const path = url.split('?')[0];
+  const query = querystring.parse(url.split('?')[1]);
 
-const server = http.createServer((req, res) => {
-  const method = req.method
-  const url = req.url
-  const path = url.split('?')[0]
-  const query = querystring.parse(url.split('?')[1])
-
-  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Content-Type', 'application/json');
 
   const resData = {
     method,
     url,
     path,
     query
-  }
+  };
 
   if (method === 'GET') {
-    res.end(JSON.stringify(resData))
+    res.end(JSON.stringify(resData));
   }
 
   if (method === 'POST') {
-    let chunkData = ''
+    let chunkData = '';
     req.on('data', chunk => {
-      chunkData += chunk.toString()
-    })
+      chunkData += chunk.toString();
+    });
     req.on('end', () => {
-      resData.postData = chunkData
-      res.end(JSON.stringify(resData))
-    })
+      resData.postData = chunkData;
+      res.end(JSON.stringify(resData));
+    });
   }
-})
+};
 
-server.listen(3000)
+module.exports = {
+  serverHandler
+};
