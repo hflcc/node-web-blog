93095ee9f7493b4c0b6bd1724ef427853e3f2256
hingbelieve@163.com <Hingbelieve@163.com>
2022-02-27 23:38:23 +0800

    feat: 新增mysql配置文件以及封装执行方法

---
+++ src/db/mysql.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql');
+const { MYSQL_CONF } = require('../conf/db');
+
+const con = mysql.createConnection(MYSQL_CONF);
+
+// 不用重复创建connection,类似单例模式
+con.connect();
+
+// 统一执行sql的函数
+function exec(sql = '') {
+	return new Promise((resolve, reject) => {
+		try {
+			con.query(sql, (err, res) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				resolve(res);
+			});
+		} catch (err) {
+			reject(err);
+		}
+	});
+}
+
+module.exports = {
+	exec
+};
