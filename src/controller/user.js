a9f4be220100695521a7c103d8d4025915f857f2
hingbelieve@163.com <Hingbelieve@163.com>
2022-03-08 22:09:06 +0800

    feat: 新增查询用户信息接口

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,3 +1,5 @@
+const { exec } = require('../db/mysql');
+
 const login = (username, password) => {
 	if (username === 'zhangsan' && password === '123') {
 		return true;
@@ -5,7 +7,17 @@ const login = (username, password) => {
 	return false;
 };
 
+// 获取用户信息
+const getUserInfo = (id) => {
+	let sql = 'select username,state,realname,id from users where 1=1 ';
+	if (id) {
+		sql += `and id='${id}'`;
+	}
+	return exec(sql);
+};
+
 
 module.exports = {
-	login
+	login,
+	getUserInfo
 };
--- src/router/user.js
+++ src/router/user.js
@@ -1,15 +1,19 @@
 const { SuccessModel, ErrorModel } = require('../model/resModel');
-const { login } = require('../controller/user');
+const { login, getUserInfo } = require('../controller/user');
 
 
-const handleUserRouter = (req, res) => {
+const handleUserRouter = async (req, res) => {
 	const method = req.method;
 	const path = req.path;
+	const query = req.query;
 
 	if (method === 'get' && path === '/api/blog/user') {
-		return {
-			msg: '这是获取用户信息接口'
-		};
+		const id = query.id;
+		if (!id) return new ErrorModel([], '查询id不能为空');
+		const res = await getUserInfo(id);
+		if (res) {
+			return new SuccessModel(res, '查询用户成功');
+		}
 	}
 
 	if (method === 'post' && path === '/api/blog/login') {
