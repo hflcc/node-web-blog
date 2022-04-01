db148b57f8f6fec26f5f6ac7213333315bfdca03
hingbelieve@163.com <Hingbelieve@163.com>
2022-04-01 14:37:48 +0800

    feat: 更新stream

---
+++ test-stream/test1.js
@@ -0,0 +1,9 @@
+// 流
+// process.stdin.pipe(process.stdout);
+
+// const http = require('http');
+// http.createServer((req, res) => {
+// 	if (req.method === 'POST') {
+// 		req.pipe(res);
+// 	}
+// }).listen(8000);
--- testFile-file/file1.js
+++ testFile-file/file1.js
@@ -2,14 +2,21 @@ const fs = require('fs');
 const path = require('path');
 
 const file = path.resolve(__dirname, './data.txt');
-
+const file1 = path.resolve(__dirname, './data-bak.txt');
 // 读取文件
-fs.readFile(file, (err, data) => {
+/*fs.readFile(file, (err, data) => {
 	if (err) {
 		console.log(err);
 		return;
 	}
 	console.log(data.toString());
+});*/
+// 读取文件,并将文件写到另外一个文件中
+const readStream = fs.createReadStream(file);
+const writeStream = fs.createWriteStream(file1);
+readStream.pipe(writeStream);
+readStream.on('end', () => {
+	console.log('传输完成');
 });
 
 // 写入文件
